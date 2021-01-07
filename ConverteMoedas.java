25a310d068b79ad2b71c82f7451d9f923d7269de
tiagoribas2021 <77054363+tiagoribas2021@users.noreply.github.com>
2021-01-07 18:13:59 -0300

    Add files via upload

---
+++ ConverteMoedas.java
@@ -0,0 +1,12 @@
+package Util;
+
+public class ConverteMoedas {
+
+	public double valorCotacao;
+	public double qtdMoeda;
+	
+	public double calculaValorTotal() {
+		return valorCotacao * qtdMoeda;
+	}	
+	
+}
